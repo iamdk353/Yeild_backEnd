eb5c6ef5e62635fea31b3cb2734ce931f4eea0eb
Dhanush Kumar M <iamdk353@gmail.com>
2025-02-05 15:32:43 +0530

    getproducts joins applied

--- src/controllers/products/getProducts.ts
+++ src/controllers/products/getProducts.ts
@@ -1,20 +1,30 @@
 import { Request, Response } from "express";
 import HandleAsync from "../../middewares/handle.Async";
 import { db } from "../../DB/connect";
-import { productsTable } from "../../DB/schema";
+import { productsTable, usersTable } from "../../DB/schema";
 
 import { ErrorResp } from "../../utils/create.error.obj";
-import { eq, not } from "drizzle-orm";
+import { eq, isNull, not } from "drizzle-orm";
+import { successResp } from "../../utils/success.obj";
 
 const getProducts = HandleAsync(async (req: Request, res: Response) => {
   const data = await db
-    .select()
+    .select({
+      productId: productsTable.productId,
+      productName: productsTable.productName,
+      price: productsTable.price,
+      quantity: productsTable.quantity,
+      buyerId: productsTable.buyerId,
+      deliveryDate: productsTable.deliveryDate,
+      buyerName: usersTable.name,
+    })
     .from(productsTable)
-    .where(not(eq(productsTable.isAccepted, true)));
-  if (data.length === 0) {
+    .where(isNull(productsTable.farmerId))
+    .leftJoin(usersTable, eq(productsTable.buyerId, usersTable.email));
+  if (data.length !== 0) {
     res.json(data);
     return;
   }
-  res.json({ code: 200, msg: data });
+  res.json(successResp("no contracts in DB"));
 });
 export default getProducts;
